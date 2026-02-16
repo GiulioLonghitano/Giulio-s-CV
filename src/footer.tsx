37c0de71c791ff0cbd59e29dbfc3ce78ab31e1e9
Giulio <giulio.longhitano@gmx.de>
2026-02-16 18:49:20 +0100

    Test1

---
+++ src/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button} from 'antd';
+
+const Footer: React.FC = () => (
+  <div style={{background: 'red'}}>
+    <p>Giulio's CV Website</p>
+    <Button>Click me</Button>
+  </div>
+);
+
+export default Footer;
